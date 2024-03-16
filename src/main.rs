7da005b2ce2ceb3d227a03524c876c8407611cda
dev-omega-lambda <dev.omega.lambda@outlook.com>
2024-03-16 11:54:52 -0300

    feat: learning -> [Cargo install cargo-edit, Complex Numbers, Flow Controle(loops)]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod r#box;
 mod csv;
 
+use std::time::{Duration, Instant};
+
+use num::Complex;
+
 use crate::csv::penguin;
 use crate::r#box::cereal;
 
@@ -10,7 +14,7 @@ fn greet_world() {
     let germany_b = "germany";
     let regions = [germany_b, japan];
 
-    for region in regions.iter() {
+    for region in IntoIterator::into_iter(regions) {
         println!("{}", &region);
     }
 }
@@ -22,6 +26,28 @@ fn main() {
 
     let a = 10;
     if a == 10 {
-        println!("Variable \"A\" is equal Ten");
+        println!("Variable \"A\" is equal Ten\n");
     }
+
+    let complex_a = Complex { re: 2.1, im: -1.2 };
+    let complex_b = Complex::new(11.1, 22.2);
+    let add_result = complex_a + complex_b;
+    println!("{} + {}i \n", add_result.re, add_result.im);
+
+    for i in 0..5 {
+        print!("Anonymous -> {} --", &i);
+        if i == 4 {
+            println!();
+        }
+    }
+
+    let mut count = 0;
+    let time_limit = Duration::new(1, 0);
+    let start = Instant::now();
+
+    while (Instant::now() - start) < time_limit {
+        count += 1;
+    }
+
+    println!("\nCount is {}", count);
 }
